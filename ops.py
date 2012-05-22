6096a1a9c11e7850e6c0436bed522fbd6e45f61a
Silas Sewell <silas@sewell.org>
2012-05-22 01:49:34 -0400

    Version 0.4.1

--- ops.py
+++ ops.py
@@ -4,7 +4,7 @@
 # This file is subject to the MIT License (see the LICENSE file).
 
 __copyright__ = '2010-2012, Silas Sewell'
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 import collections
 import datetime
